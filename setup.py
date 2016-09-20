969c23e4c2c174ddbfffce32e61e28cb73a94746
Maurits van Rees <maurits@vanrees.org>
2016-09-20 15:58:07 +0200

    Reindented setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,35 +2,36 @@ from setuptools import setup, find_packages
 
 version = '1.0.14.dev0'
 
-setup(name='plone.stringinterp',
-      version=version,
-      description="Adaptable string interpolation",
-      long_description=open("README.txt").read() + "\n" +
-                       open("CHANGES.txt").read(),
-      classifiers=[
-          "Environment :: Web Environment",
-          "Framework :: Plone",
-          "Framework :: Zope2",
-          "License :: OSI Approved :: GNU General Public License (GPL)",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-        ],
-      keywords='Plone Zope Interpolation',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.stringinterp',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Products.CMFCore',
-          'zope.i18n',
-      ],
-      entry_points="""
-          [z3c.autoinclude.plugin]
-          target = plone
-      """,
-      )
+setup(
+    name='plone.stringinterp',
+    version=version,
+    description="Adaptable string interpolation",
+    long_description=(open("README.txt").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Plone",
+        "Framework :: Zope2",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords='Plone Zope Interpolation',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.stringinterp',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Products.CMFCore',
+        'zope.i18n',
+    ],
+    entry_points="""
+        [z3c.autoinclude.plugin]
+        target = plone
+    """,
+)
