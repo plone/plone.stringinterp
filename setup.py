36cbb327db0dab8392f6246fe90dd3d982c3d27a
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:42:20 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     keywords='Plone Zope Interpolation',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.stringinterp',
+    url='https://pypi.org/project/plone.stringinterp',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone'],
